75b9325ffb982383b5d494184c75d428824062bf
Amar Landa <al@amarlanda.com>
2017-10-30 11:41:31 +0000

    automated push 10/30/2017 11:41:30

--- Audit/bulid-HTML.ps1
+++ Audit/bulid-HTML.ps1
@@ -13,7 +13,7 @@ $a = $a + "</style>"
 
 $HTML = $Allservers | select  MachineType, ou, name, 'VI - VC', 'VI - VMPath' | ConvertTo-HTML -head $a -body "<H2>FINANCIAL TIMES</H2>"
 
-$HTMLlength= $html.count 
+$HTMLlength= $html.count
 
 $Allservers | % {
   $currentserver = $_
@@ -23,6 +23,27 @@ $Allservers | % {
     $html[$i]  = $html[$i] -replace $servername, $hyperlink
   }
 }
+
+
+$FTServers | ? { $_.psremoting -eq $true} | % {
+
+$one = Import-Clixml -Path "C:\Amar\Computers\$($_.name).xml"|
+ConvertTo-HTML -Fragment
+
+$two = Import-Clixml -Path "C:\Amar\Computers\$($_.name)-Installedsoftware.xml"|
+ConvertTo-HTML -Fragment
+
+
+$html = ConvertTo-HTML -head $a -Body "<h1>FINANCIAL TIMES</h1><h3>General</h3> $one <h3>Installed Software</h3> $two" -Title "Server Stats"
+
+$html = $html -replace "\<table\>",'<table cellpadding="10">'
+
+$html = $html -replace '(?m)\s+$', "`r`n<BR>"
+
+$html| Out-File "C:\Amar\Computers\$($_.name)-status.html"
+
+}
+
 
 <#
     $html = $HTML | %{
